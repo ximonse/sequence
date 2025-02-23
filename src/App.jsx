288fceb36d3b4c8c9a8e9dafd88391120f5032cc
Simon Zetterberg <ximon.se@gmail.com>
2025-02-23 16:47:17 +0100

    Restore project files to GitHub

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Hello Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
