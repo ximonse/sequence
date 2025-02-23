3e7869d04b8123677bd6b3884796c8027b800943
Simon Zetterberg <ximon.se@gmail.com>
2025-02-23 18:38:50 +0100

    Fix: Updated Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // ✅ Ensures Tailwind scans your files
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
